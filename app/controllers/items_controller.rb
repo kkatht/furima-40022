232ed7b7fd606525434216e6384779af149c99cc
kkatht <kkatht@icloud.com>
2024-03-10 23:41:22 +0900

    商品情報編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
 
     def index
       @items = Item.all.order('created_at DESC')
@@ -21,6 +21,23 @@ class ItemsController < ApplicationController
 
     def show
       @item = Item.find(params[:id])
+    end
+
+    def edit
+      @item = Item.find(params[:id])
+      if current_user.id != @item.user_id
+        redirect_to root_path
+      end
+    end 
+
+
+    def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+        redirect_to root_path
+      else
+        render :edit, status: :unprocessable_entity
+      end 
     end
 
 
